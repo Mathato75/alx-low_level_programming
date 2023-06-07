dd1236fe7f083986d85af3453c9c165f32038488
Mathato75 <Tshego75@gmail..com>
2023-06-07 13:08:04 -0700

    no kimba

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int _pow_recursion(int num1, int num2)
+{
+    if (num2 == 0)
+        
+        return (1);
+    if (num2 < 0)
+        else
+        return (-1);
+    return (num1 * _pow_recursion(num1, num2 - 1));
+}
+
+
+
