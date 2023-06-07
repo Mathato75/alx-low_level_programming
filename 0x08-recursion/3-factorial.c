03102b4da9447359d4378437e206b7e74d236cff
Mathato75 <Tshego75@gmail..com>
2023-06-07 13:17:24 -0700

    thhfh

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
-int factorial(int num)
+int _pow_recursion(int num1, int num2)
 {
-    if (num < 0)
-        return (-1);
-    if (num == 0);
+    if (num2 == 0)
+        
         return (1);
-    else
-
-    return (num * factorial(num - 1));
+    if (num2 < 0)
+    
+        return (-1);
+    return (num1 * _pow_recursion(num1, num2 - 1));
 }
-
-
-
 
 
