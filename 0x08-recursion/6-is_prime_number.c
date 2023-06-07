b6dab0fae150675e1aab422a468e943dc82e829a
Mathato75 <Tshego75@gmail..com>
2023-06-07 14:08:00 -0700

    see

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-int prime(int num1, int num2);
+int actual_prime(int num1, int num2);
+
 
 int is_prime_number(int num1)
 {
@@ -8,15 +9,19 @@ int is_prime_number(int num1)
 
         return (0);
 
-    return (aprime(num1, num1 - 1));
+    return (actual_prime(num1, num1 - 1));
 }
 
-9int aprime(int num1, int num2)
+
+int actual_prime(int num1, int num2)
 {
-    if (num1 == num2)
-        return (1);
-    if (num1 % num2 == 0 && num2 > 0)
-        return (0);
-    return (aprime(num1, num2 - 1));
-}
+    if (num1 == 1)
 
+        return (1);
+
+    if (num1 % num2 == 0 && num2 > 0)
+
+        return (0);
+
+    return (actual_prime(num1, num2 - 1));
+}
