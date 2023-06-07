b7b306abbc4a0be1d1df59966cae9dc5f7124486
Mathato75 <Tshego75@gmail..com>
2023-06-07 13:22:18 -0700

    hmm

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,13 +1,15 @@
+
 #include "main.h"
 
 int _pow_recursion(int num1, int num2)
 {
-    if (num2 == 0)
-        
-        return (1);
     if (num2 < 0)
-        else
+    
+	    /* if this doesnt work none will */
         return (-1);
+    if  (num2== 0)
+
+    	    return (1);
     return (num1 * _pow_recursion(num1, num2 - 1));
 }
 
