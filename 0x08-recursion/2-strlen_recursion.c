3bed05cacd7f9002285042f99b497dbffce21960
Mathato75 <Tshego75@gmail..com>
2023-06-07 10:36:12 -0700

    help

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,18 +2,17 @@
 
 int _strlen_recursion(char* s)
 {
-    int string1_length = string_length(string1);
+    
 
-    // print out the calculated length to verify it is 5
-    printf("length: %d\n", string1_length);
+ 
 
-    return 0;
-
-    int string_length(char*s)
+    int string_length(char *string)
     {
-        
+        int string1_length = string_length(string1);
+
         if (*string == '\0') return 0;
         else return 1 + string_length(string + 1);
+        return (string);
     }
 
 }
