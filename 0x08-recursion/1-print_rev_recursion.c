68f9c0ba34c211da3b08b3c1a0ba82eceeb1e7a0
Mathato75 <Tshego75@gmail..com>
2023-06-07 10:18:29 -0700

    print

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include main.h
+
+
+//The print rev recursion trying and winning it
+
+/*int_rev_recursion.c*/
+
+
+void _print_rev_recursion(char* s)
+{
+    if (*s == '\0')
+        return;
+    else
+    {
+        _print_rev_recursion(a + 1);
+    }
+}
+
