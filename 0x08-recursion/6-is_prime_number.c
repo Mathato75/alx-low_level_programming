3c58f4a59de1754b5a284f10a847644d897286f2
Mathato75 <Tshego75@gmail..com>
2023-06-07 13:54:52 -0700

    lets see

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -13,10 +13,10 @@ int is_prime_number(int num1)
 
 int prime(int num1, int num2)
 {
-    if (i == 1)
+    if (num1 == num2)
         return (1);
-    if (n % i == 0 && i > 0)
+    if (num1 % num2 == 0 && num2 > 0)
         return (0);
-    return (aprime(n, i - 1));
+    return (aprime(num1, num2 - 1));
 }
 
